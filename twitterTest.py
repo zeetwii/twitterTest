48fd9ac28f8650de480b7c95850da9fca3a5d72e
Nick Ashworth <35108039+zeetulsa@users.noreply.github.com>
2021-09-27 14:19:01 -0400

    test

--- twitterTest.py
+++ twitterTest.py
@@ -10,14 +10,26 @@ import sys # needed to exit
 import tkinter as tk # needed for dialog
 from tkinter import filedialog as fd # needed for dialog
 
-class rbStreamListener(tweepy.StreamListener):
+class ReplyListener(tweepy.Stream):
     """
     Class that handles tweepy events
     """
     
+    def on_connect(self):
+        print("stream connected")
+        return super().on_connect()
+    
     def on_status(self, status):
         print(status.text)
         
+    def on_connection_error(self):
+        self.disconnect()
+        
+    def on_data(self, raw_data):
+        print(str(raw_data))
+    
+    def filter(self, *, follow=None, track=None, locations=None, filter_level=None, languages=None, stall_warnings=False, threaded=False):
+        return super().filter(follow=follow, track=track, locations=locations, filter_level=filter_level, languages=languages, stall_warnings=stall_warnings, threaded=threaded)        
     
 
 class ReplyBot:
@@ -41,6 +53,11 @@ class ReplyBot:
 
         self.api = tweepy.API(self.auth)
         
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+        self.accessToken = accessToken
+        self.accessTokenSecret = accessTokenSecret
+        
     def validateBot(self):
         """
         Validates that the credientals are working on the bot
@@ -56,13 +73,37 @@ class ReplyBot:
             return False
         
     def startStream(self):
+        print("Starting stream, press Ctrl + C to exit")
         
         try:
-            self.stream = tweepy.Stream(auth= self.api.auth, listener= rbStreamListener)
-            self.stream.filter(follow=[str(self.api.me().id)])
+            #self.stream = ReplyBot(self.apiKey, self.apiSecret, self.accessToken, self.accessTokenSecret)
+            self.stream = tweepy.Stream(self.apiKey, self.apiSecret, self.accessToken, self.accessTokenSecret)
+            self.stream.sample()
+            
+            #print(self.stream.running)
         except KeyboardInterrupt:
             print("Keyboard interrupt")
+            self.stream.disconnect()
             sys.exit(0)
+            
+    def test(self):
+        print(str(self.api.get_direct_messages()))
+        target_id = 1288710030880104448
+        
+        options = [
+            {
+              "label": "I'm good",
+              "description": "It means you're doing good",
+              "metadata": "external_id_1"
+            },
+            {
+              "label": "Not so good",
+              "description": "It means you're not doing good",
+              "metadata": "external_id_2"
+            }
+          ]
+        
+        #self.api.send_direct_message(target_id, "Quick reply Test", quick_reply_options=options)
             
         
 
@@ -86,12 +127,6 @@ if __name__ == "__main__":
         sys.exit("Invalid configs")
         
     replyBot.startStream()
-
+    #replyBot.test()
     
-    #api.update_status("Test Tweet")
-    
-    #test = api.list_direct_messages(5)
-    #print(api.me().id)
-    
-    #print(test)
     
